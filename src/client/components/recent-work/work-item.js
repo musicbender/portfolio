bbb14f2ba6d512b36280dddcfebde6b33efe9b9b
Pat Jacobs <musicbender@gmail.com>
2019-03-30 15:23:07 -0700

    Working work items parallax with plx

--- src/client/components/recent-work/work-item.js
+++ src/client/components/recent-work/work-item.js
@@ -1,48 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Parallax } from 'react-scroll-parallax';
+import Plx from 'react-plx';
 import Button from '../_global/button';
 import cn from 'classnames/bind';
 import style from './work-item.css';
 const cx = cn.bind(style);
 
 const WorkItem = ({ item, index }) => {
-  const paraConf = {
-    image: [[0, 0], [20, -20]],
-    info: [[0, 0], [20, -20]]
+  const baseEnd = 1400;
+  const accumulator = 535;
+
+  const getPlxData = (item, i) => {
+    return [
+      {
+        start: 'self',
+        end: baseEnd + (accumulator * index),
+        properties: [
+          {
+            startValue: item.startValue,
+            endValue: 0,
+            unit: 'em',
+            property: 'translateY'
+          }
+        ]
+      }
+    ];
   }
 
   const renderImageSection = () => {
-    return item.parallax.image.map((seg, i) => {
+    return item.plx.image.map((seg, i) => {
       return (
-        <Parallax
+        <Plx
           className={cx(style.parallax, style[`index-${i}`])}
-          x={[0, 0]}
-          y={paraConf.image[i] || [0, 0]}
+          parallaxData={getPlxData(seg, i)}
           key={`${i}` + item.title + 'image' + JSON.stringify(seg)}
         >
-          <div className={cx(style.wrapper)}>
-            <div className={cx(style.imageCover)}></div>
+          <div className={cx(style.parallaxInner)}>
+            <div className={cx(style.wrapper)}>
+              <div className={cx(style.imageCover)}></div>
+            </div>
           </div>
-        </Parallax>
+        </Plx>
       );
     });
   }
 
   const renderInfoSection = () => {
-    return item.parallax.info.map((seg, i) => {
+    return item.plx.info.map((seg, i) => {
       return (
-        <Parallax
+        <Plx
           className={cx(style.parallax, style[`index-${i}`])}
-          x={[0, 0]}
-          y={paraConf.info[i] || [0, 0]}
+          parallaxData={getPlxData(seg, i)}
           key={`${i}` + item.title + 'info' + JSON.stringify(seg)}
         >
-          <div className={cx(style.wrapper)}>
-            <h4 className={cx(style.title)}>{item.title}</h4>
-            <p className={cx(style.description)}>{item.description}</p>
+          <div className={cx(style.parallaxInner)}>
+            <div className={cx(style.wrapper)}>
+              <h4 className={cx(style.title)}>{item.title}</h4>
+              <p className={cx(style.description)}>{item.description}</p>
+            </div>
           </div>
-        </Parallax>
+        </Plx>
       );
     });
   }
