5756a904690289fc627528db3d136bbf6dfd84dd
Pat Jacobs <musicbender@gmail.com>
2019-05-24 13:49:54 -0700

    add about me dot grid animation with react hooks

--- src/client/components/about-me/about-me.js
+++ src/client/components/about-me/about-me.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Plx from 'react-plx';
 import Heading from '../_global/heading';
+import { DotGrid } from '../_particles';
 import config from './config.json';
-import { hasWindow } from '../../util/util';
+import { hasWindow, countLongestArray } from '../../util/util';
+import { startSequence } from '../../util/animation';
+import { dotGridA } from './dots';
 import cn from 'classnames/bind';
 import style from './about-me.css';
 const cx = cn.bind(style);
 
 const AboutMe = () => {
+  const [ dotGridIndex, updateSequence ] = useState(0);
+  const interval = 180;
+  const delay = 4000;
+
+  const handleIndex = (index) => {
+    updateSequence(index)
+  }
+
+  // on mount
+  useEffect(() => {
+    const dotGridLength = countLongestArray([
+      dotGridA
+    ]);
+
+    startSequence({
+      length: dotGridLength,
+      interval: interval,
+      delay: delay,
+      index: dotGridIndex
+    }, handleIndex);
+  }, []);
+
   return (
     <div className={cx(style.aboutMe)}>
       <Heading text={config.heading} />
+      <DotGrid
+        classNames={cx(style.dotGridA)}
+        sequence={dotGridA}
+        index={dotGridIndex}
+      />
       <Plx
         disabled={!hasWindow()}
         parallaxData={[{
