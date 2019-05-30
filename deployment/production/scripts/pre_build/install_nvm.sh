a818954f79a27a920ab6e7be2a85155df160a105
Pat Jacobs <musicbender@gmail.com>
2019-05-30 01:10:26 -0700

    nvm issues on build fix

--- deployment/production/scripts/pre_build/install_nvm.sh
+++ deployment/production/scripts/pre_build/install_nvm.sh
@@ -4,5 +4,5 @@ if [ $? -ne 0 ]
 then
    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
    . ~/.nvm/nvm.sh
-   nvm install 10.13.0
+   sudo nvm install 10.13.0
 fi
