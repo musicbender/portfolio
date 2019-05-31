a69b41e38fde66bb4831117d23ce6db142eadbca
Pat Jacobs <musicbender@gmail.com>
2019-05-31 01:13:55 -0700

    fix pm2 more

--- deployment/production/scripts/application_stop/stop_pm2.sh
+++ deployment/production/scripts/application_stop/stop_pm2.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+source ~/.bashrc
 source /home/ubuntu/.profile
 cd /var/www/portfolio
 pm2 kill
