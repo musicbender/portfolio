d5a64030f9c1ab64a05e7b703cebbedbd75c129a
Pat Jacobs <musicbender@gmail.com>
2019-05-31 00:09:38 -0700

    test 2

--- deployment/production/scripts/application_start/start_pm2.sh
+++ deployment/production/scripts/application_start/start_pm2.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 cd /var/www/portfolio
 pm2 start deployment/production/app-production.json --interpreter node_modules/babel-cli/bin/babel-node.js
--- deployment/production/scripts/application_stop/stop_pm2.sh
+++ deployment/production/scripts/application_stop/stop_pm2.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 cd /var/www/portfolio
+pm2 kill
