75fc1fb07f552407cfb71f603501cd8ac06e8b7d
Pat Jacobs <musicbender@gmail.com>
2019-05-31 14:49:14 -0700

    try nvm use

--- deployment/production/scripts/application_stop/stop_pm2.sh
+++ deployment/production/scripts/application_stop/stop_pm2.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo 1
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 source ~/.bashrc
 source /home/ubuntu/.profile
 cd /var/www/portfolio
-/home/ubuntu/.nvm/versions/node/v10.13.0/bin/pm2 kill
+nvm use 10.13.0
+pm2 kill
