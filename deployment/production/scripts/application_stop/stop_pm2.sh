68e060ee0a24c7a960acb948cfe7d6ac4d631465
Pat Jacobs <musicbender@gmail.com>
2019-05-31 15:27:40 -0700

    more try why oh why

--- deployment/production/scripts/application_stop/stop_pm2.sh
+++ deployment/production/scripts/application_stop/stop_pm2.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 source ~/.bashrc
 source /home/ubuntu/.profile
 cd /var/www/portfolio
-nvm use 10.13.0
 pm2 kill
