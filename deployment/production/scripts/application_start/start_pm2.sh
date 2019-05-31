0e2476e022994360b8cba57bdc25ac9803cc8ace
Pat Jacobs <musicbender@gmail.com>
2019-05-31 16:13:30 -0700

    change pm2 config

--- deployment/production/scripts/application_start/start_pm2.sh
+++ deployment/production/scripts/application_start/start_pm2.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-source /home/ubuntu/.profile
 cd /var/www/portfolio
-pm2 start deployment/production/app-production.json --interpreter node_modules/babel-cli/bin/babel-node.js
+pm2 start deployment/production/app-production.json --interpreter node_modules/@babel/node/lib/babel-node.js
