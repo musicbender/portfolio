de5839869fed300eaaefd84a990281cd804a97a0
Pat Jacobs <musicbender@gmail.com>
2019-05-31 20:29:34 -0700

    troubleshooting aws secret gathering

--- deployment/lib/get-env.js
+++ deployment/lib/get-env.js
@@ -2,15 +2,24 @@ const AWS = require('aws-sdk');
 
 module.exports = function (SecretId, region = 'us-east-2') {
   const secretsmanager = new AWS.SecretsManager({
-    apiVersion: '2017-10-17', region,
+    apiVersion: '2017-10-17',
+    region
   });
 
   const params = { SecretId };
+
+  console.log(`aws params:`);
+  console.log(params);
 
   return secretsmanager
     .getSecretValue(params)
     .promise()
     .then(function (result) {
+      console.log(`result:`);
+      console.log(result);
       return JSON.parse(result.SecretString);
+    })
+    .catch(function (err) {
+      throw new Error(err);
     });
 };
