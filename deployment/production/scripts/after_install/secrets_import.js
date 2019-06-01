6f6a1690ab73cce3eb8b97dfe5ae49295b1a3182
Pat Jacobs <musicbender@gmail.com>
2019-05-31 18:22:06 -0700

    fix app secrets fetching

--- deployment/production/scripts/after_install/secrets_import.js
+++ deployment/production/scripts/after_install/secrets_import.js
@@ -1,8 +1,10 @@
-const getEnv = require('../../../lib/get-env');
-const config = require('../../app-production');
+const getEnv = require('../../lib/get-env');
+const config = require('../app-production');
 
 getEnv('portfolio-live')
   .then(function (env) {
+    console.log(`envs returned:`);
+    console.log(env);
     let app = config.apps[0];
     Object.keys(env).forEach(function (key) {
       app.env[key] = env[key];
